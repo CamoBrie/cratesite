50219a77eb0fd265c19fb6a33fd81c54954d95d8
CamoBrie <bvmeggelen@gmail.com>
2021-03-13 22:30:57 +0100

    changed name

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,8 +1,14 @@
 const IDName = {
-	0: 'dirt key',
+	0: 'gravel key',
 	1: 'stone key',
-	2: 'iron key',
-	3: 'diamond key',
+	2: 'redstone key',
+	3: 'lapis key',
+	4: 'iron key',
+	5: 'gold key',
+	6: 'diamond key',
+	7: 'emerald key',
+	8: 'obsidian key',
+	9: 'bedrock key',
 };
 const updatetime = 50;
 
